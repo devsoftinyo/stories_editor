829fc46f340ca37086332cda147ae75d951aebfc
Bego-db <120643757+Bego-db@users.noreply.github.com>
2023-05-25 17:03:00 +0300

    Update color_selector.dart

--- lib/src/presentation/widgets/color_selector.dart
+++ lib/src/presentation/widgets/color_selector.dart
@@ -15,73 +15,78 @@ class ColorSelector extends StatelessWidget {
     return Consumer3<ControlNotifier, TextEditingNotifier, PaintingNotifier>(
       builder:
           (context, controlProvider, editorProvider, paintingProvider, child) {
-        return Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            /// current selected color
-            Container(
-              height: 120.h,
-              width: 120.w,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
+        return Container(
+          height: screenUtil.screenWidth * 0.1.h,
+          width: screenUtil.screenWidth,
+          alignment: Alignment.center,
+          padding: const EdgeInsets.only(left: 5, right: 5).r,
+          child: Row(
+            children: [
+              /// current selected color
+              Container(
+                height: 50.h,
+                width: 50.w,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: controlProvider.isPainting
+                        ? controlProvider.colorList![paintingProvider.lineColor]
+                        : controlProvider.colorList![editorProvider.textColor],
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.white, width: 1.5.w)),
+                child: ImageIcon(
+                  const AssetImage('assets/icons/pickColor.png',
+                      package: 'stories_editor'),
                   color: controlProvider.isPainting
-                      ? controlProvider.colorList![paintingProvider.lineColor]
-                      : controlProvider.colorList![editorProvider.textColor],
-                  shape: BoxShape.circle,
-                  border: Border.all(color: Colors.white, width: 1.5.w)),
-              child: ImageIcon(
-                const AssetImage('assets/icons/pickColor.png',
-                    package: 'stories_editor'),
-                color: controlProvider.isPainting
-                    ? (paintingProvider.lineColor == 0
-                        ? Colors.black
-                        : Colors.white)
-                    : (editorProvider.textColor == 0
-                        ? Colors.black
-                        : Colors.white),
-                size: 20.r,
-              ),
-            ),
-
-            /// color list
-            Expanded(
-              child: SingleChildScrollView(
-                padding: EdgeInsets.zero,
-                physics: const BouncingScrollPhysics(),
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    ...controlProvider.colorList!.map((color) {
-                      final int index =
-                          controlProvider.colorList!.indexOf(color);
-                      return AnimatedOnTapButton(
-                        onTap: () {
-                          if (controlProvider.isPainting) {
-                            paintingProvider.lineColor = index;
-                          } else {
-                            editorProvider.textColor = index;
-                          }
-                        },
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 5.w),
-                          child: Container(
-                            height: 50.h,
-                            width: 50.w,
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                                color: controlProvider.colorList![index],
-                                shape: BoxShape.circle,
-                                border: Border.all(
-                                    color: Colors.white, width: 1.5.w)),
-                          ),
-                        ),
-                      );
-                    })
-                  ],
+                      ? (paintingProvider.lineColor == 0
+                          ? Colors.black
+                          : Colors.white)
+                      : (editorProvider.textColor == 0
+                          ? Colors.black
+                          : Colors.white),
+                  size: 20.r,
                 ),
               ),
-            ),
-          ],
+
+              /// color list
+              Expanded(
+                child: SingleChildScrollView(
+                  padding: EdgeInsets.zero,
+                  physics: const BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      ...controlProvider.colorList!.map((color) {
+                        final int index =
+                            controlProvider.colorList!.indexOf(color);
+                        return AnimatedOnTapButton(
+                          onTap: () {
+                            if (controlProvider.isPainting) {
+                              paintingProvider.lineColor = index;
+                            } else {
+                              editorProvider.textColor = index;
+                            }
+                          },
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 5.w),
+                            child: Container(
+                              height: 50.h,
+                              width: 50.w,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                  color: controlProvider.colorList![index],
+                                  shape: BoxShape.circle,
+                                  border: Border.all(
+                                      color: Colors.white, width: 1.5.w)),
+                            ),
+                          ),
+                        );
+                      })
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         );
       },
     );
